7af0212091c8d11e6759decdf6761316f536df50
saigithubcharan <battusaicharan@gmail.com>
2024-12-10 11:25:38 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import Q1Signin from './react_templates/Q1Signin'
-import Q2task from './react_templates/Q2task'
-import Q3task from './react_templates/Q3task'
-import Q4task from './react_templates/Q4task'
-import Q5task from './react_templates/Q5task'
+
 function App() {
   return (
     <div>
